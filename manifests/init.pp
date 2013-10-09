90210baa2939d2a3f11d8f94bc6032137108cc7b
Joshua Rountree <rountrjf@ucmail.uc.edu>
2013-10-09 09:25:14 -0400

    added initial manifest code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Filezilla Client to /Applications.
+#
+# include filezilla
+class filezilla {
+  package { 'filezilla':
+    provider => 'compressed_app',
+    source => 'http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.7.3/FileZilla_3.7.3_i686-apple-darwin9.app.tar.bz2',
+  }
 }
